1d6a54af719f078993e7881934af76a094492b88
Gabriel de Ioannes Becker <gdeioannes@gmail.com>
2017-11-11 18:56:05 -0300

    Loading Test with sounds

---
+++ Assets/Sprites/ControlDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDoor : MonoBehaviour {
+	public GameObject animation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			animation.SetActive(true);
+		}
+	}
+}
