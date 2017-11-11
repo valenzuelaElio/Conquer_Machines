717cc56a05a43a1ba63b3248d4d3a0d48c5f85a6
Gabriel de Ioannes Becker <gdeioannes@gmail.com>
2017-11-11 16:46:31 -0300

    Changes Door

---
+++ Assets/Sprites/AnimationEventTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventTest : MonoBehaviour {
+
+	public ParticleSystem particle1;
+	public ParticleSystem particle2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void animationTest1(){
+		Debug.Log("Hola Mundo eventx");
+		particle1.Play();
+		particle2.Play();
+	}
+}
